70d8e35a09cc2f42514c55afd32832c7c98a09f5
IamHimOmar <omar0954@edu.eal.dk>
2016-08-03 12:25:17 +0200

    npm jenkins mocha
    package added

--- src/api/bankTransactions.js
+++ src/api/bankTransactions.js
@@ -6,3 +6,29 @@ var pg = require('pg');
 var conString = function() {
     return process.env.NODE_ENV == "test" ? "postgres://omarmhadden:eal281095@localhost:5432/dumpdb":"postgres://omarmhadden:eal281095@localhost:5432/Omar";
 }
+
+router.post("/user",Withdraw(req,res));
+
+
+function Withdraw(req,res){
+  //requires amount, description, date, account id
+    var conn = conString();
+    pg.connect(conn,function(err,client,done){
+      client.query("ALTER testing.bank_balance (amount,date) values ((amount - $1),now())",[amount]);
+      var query = client.query("INSERT INTO testing.bank_transaction (name) VALUES ($1)",[req.body.name]);
+      query.on('end',function(){
+          res.json({name:req.body.name});
+      });
+    }
+}
+
+function Deposite(){
+  //requires amount, description, date
+}
+
+function Transfer(){
+  //requires amount, description, date, account tranfered to
+}
+function OnlinePayment(){
+  //requires amount, description, date
+}
